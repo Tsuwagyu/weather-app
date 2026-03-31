81093b8ca0d21425eea36c87675acff843c2452c
mercury-Hg80 <150161680+mercury-Hg80@users.noreply.github.com>
2026-03-31 16:41:34 -0400

    add search functionality to searchbox, add try catch statement components

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-async function weatherFetch() {
-    const searchBoxRef = document.querySelector(input[type="search"]);
+async function weatherFetch(location, date1, date2) {
 
     try {
 
-        const response = await fetch ('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/[location]/[date1]/[date2]?key=8AAVWB7V3JELBLHF38KT3VGLC');
+        const response = await fetch (`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/${date1}/${date2}?key=8AAVWB7V3JELBLHF38KT3VGLC`);
+        const result = await response.json();
 
+        console.log(result);
+
+
+    } catch(error) {
+        console.error('there was a problem', error);
     }
 
 
+}
+
+function searchLocale() {
+    let locationSearchRef = document.querySelector('#location-search');
+
+    locationSearchRef.addEventListener('keydown', (e) => {
+        if (e.key === 'Enter') {
+            console.log('this is working');
+        }
+    })
 }
