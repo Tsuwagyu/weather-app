b3c616e3f6119f59a5cc2097bb232f07b7aef7d0
mercury-Hg80 <150161680+mercury-Hg80@users.noreply.github.com>
2026-03-31 16:08:18 -0400

    add basic async function skeleton with try catch statement

---
+++ script.js
@@ -0,0 +1,11 @@
+async function weatherFetch() {
+    const searchBoxRef = document.querySelector(input[type="search"]);
+
+    try {
+
+        const response = await fetch ('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/[location]/[date1]/[date2]?key=8AAVWB7V3JELBLHF38KT3VGLC');
+
+    }
+
+
+}
