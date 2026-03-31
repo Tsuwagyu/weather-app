782e91e0cfde851053c75913f4a041aa69f8a1ff
mercury-Hg80 <150161680+mercury-Hg80@users.noreply.github.com>
2026-03-31 18:04:06 -0400

    feat: necessaryResults function to return streamlined weather info

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ async function weatherFetch(location, date1, date2) {
         const response = await fetch (`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/${date1}/${date2}?key=8AAVWB7V3JELBLHF38KT3VGLC`);
         const result = await response.json();
 
-        console.log(result);
+
+        let finalData = necessaryResults(result);
+        console.log(finalData);
 
 
     } catch(error) {
@@ -23,4 +25,23 @@ function searchLocale() {
             console.log('this is working');
         }
     })
+}
+
+function necessaryResults(result) {
+            
+    return {
+        alerts: result.alerts,
+        location: result.resolvedAddress,
+        temperature: result.currentConditions.temp,
+        conditions: result.currentConditions.conditions,
+        feel: result.currentConditions.feelslike,
+        precipitation: result.currentConditions.precip,
+        uvIndex: result.currentConditions.uvindex,
+        sunrise: result.currentConditions.sunrise,
+        sunset: result.currentConditions.sunset, 
+        windsp: result.currentConditions.windspeed,
+        windgus: result.currentConditions.windgust,
+                
+
+    };
 }
