1b51d135e49b1033423a310edd834949ae999266
mercury-Hg80 <150161680+mercury-Hg80@users.noreply.github.com>
2026-04-07 15:03:57 -0400

    feat: weather info displays to user and clears when new location is searched

--- script.js
+++ script.js
@@ -4,11 +4,33 @@ async function weatherFetch(location) {
 
         const response = await fetch (`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=8AAVWB7V3JELBLHF38KT3VGLC`);
         const result = await response.json();
-        let weatherInfoDiv = document.querySelector('.weather-results');
+        let resultsRef = document.querySelector('.weather-results');
 
 
         let finalData = necessaryResults(result);
         console.log(finalData);
+
+        resultsRef.innerHTML = ``;
+
+        Object.entries(finalData).forEach(([key, value]) => {
+
+            const p = document.createElement('p');
+
+            if(Array.isArray(value)) {
+                if(value.length === 0) {
+                    p.textContent = "No present alerts";
+                } else {
+                    p.textContent = value.map(alert => alert.event).join(', ');
+                }
+                
+            } else {
+                p.textContent = `${key}: ${value}`;
+            }
+            p.classList.add(`weather-${key}`);
+            resultsRef.append(p);
+            
+        });
+
 
 
 
@@ -23,29 +45,30 @@ async function weatherFetch(location) {
 
 function searchLocale() {
     let locationSearchRef = document.querySelector('#location-search');
+    let formRef = document.querySelector('.weather-form');
 
-    locationSearchRef.addEventListener('keydown', (e) => {
-        if (e.key === 'Enter') {
-            console.log('this is working');
-            weatherFetch(locationSearchRef.value);
-        }
+    formRef.addEventListener('submit', (e) => {
+        e.preventDefault();
+        console.log('this is working');
+        weatherFetch(locationSearchRef.value);
+        
     })
 }
 
 function necessaryResults(result) {
             
     return {
-        alerts: result.alerts,
-        location: result.resolvedAddress,
-        temperature: result.currentConditions.temp,
-        conditions: result.currentConditions.conditions,
-        feel: result.currentConditions.feelslike,
-        precipitation: result.currentConditions.precip,
-        uvIndex: result.currentConditions.uvindex,
-        sunrise: result.currentConditions.sunrise,
-        sunset: result.currentConditions.sunset, 
-        windsp: result.currentConditions.windspeed,
-        windgus: result.currentConditions.windgust,
+        Alerts: result.alerts,
+        Location: result.resolvedAddress,
+        Temperature: result.currentConditions.temp,
+        Conditions: result.currentConditions.conditions,
+        Feel: result.currentConditions.feelslike,
+        Precipitation: result.currentConditions.precip,
+        UV: result.currentConditions.uvindex,
+        Sunrise: result.currentConditions.sunrise,
+        Sunset: result.currentConditions.sunset, 
+        Windspeeds: result.currentConditions.windspeed,
+        Windgusts: result.currentConditions.windgust,
                 
 
     };
